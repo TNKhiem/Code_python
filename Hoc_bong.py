ee662c465ac64d98fb760799e3f433a1d0c488c4
TNKhiem <95389597+TNKhiem@users.noreply.github.com>
2021-12-06 22:08:25 +0700

    sửa lại phần chọn sinh viên

--- Hoc_bong.py
+++ Hoc_bong.py
@@ -12,25 +12,29 @@ def bangDiem(soSV):
     for i in range(soSV):
         list[i]=[" ",0,0,0]
     return list
-tenMon=["Toan cao cap","Gioi thieu nganh","Tu duy lap trinh","Kinh te vi mo"]
+def hocBong(SV):
+    ds=[]
+    while len(ds)<5:   #so luong hoc sinh nhan hoc bong
+        M=max(SV,key=lambda person : person[3])
+        ds.append(M)
+        SV.remove(M)
+    return ds
+tenMon=["Toan cao cap","Gioi thieu nganh"]
 dsTC=[]
 soSV=int(input("Nhap so sinh vien: "))
 SV=bangDiem(soSV)
-n=len(tenMon)
-print(soSV)
-print(SV)
-dsTC=[3,2,3,3]
+dsTC=[3,2]    # danh sách so tin chi moi mon
 for i in range(soSV):
     SV[i][0]=input("Nhap ten sinh vien: ")
-    dsDiem=[0]*n
-    for j in range(n):
+    dsDiem=[0]*len(tenMon)
+    for j in range(len(tenMon)):
         dsDiem[j]=float(input("Nhap diem mon "+str(tenMon[j])+" : "))
     SV[i][1]=tinhDiemTB(dsDiem,dsTC)
     SV[i][2]=int(input("Nhap diem ren luyen: "))
     SV[i][3]=SV[i][1]+0.2*SV[i][2]
+dsHocBong=hocBong(SV)
+print("Danh sach nhan hoc bong la:")
+for i in range(5):
+    print(dsHocBong[i][0])
 
-for i in SV:
-    print(i)
-hocBong=sorted(SV, key=lambda person:person[3])
-for i in reversed(range(5)):
-    print(SV[i][0])
+
