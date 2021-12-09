8d157e62e8931f2d89acc10f19b826a8fc4951e9
TNKhiem <95389597+TNKhiem@users.noreply.github.com>
2021-12-09 07:39:46 +0700

    Bổ sung ngoại lệ số SV < số SV nhận học bổng

--- Hoc_bong.py
+++ Hoc_bong.py
@@ -1,9 +1,8 @@
 # Ham tinh diem trung binh cac mon
 def tinhDiemTB(dsDiem,dsTC):
-    n=len(dsTC)
     tongDiem=0
     tongTC=0
-    for i in range(n):
+    for i in range(len(dsTC)):
         tongDiem=tongDiem+dsDiem[i]*dsTC[i]
         tongTC=tongTC+dsTC[i]
     diemTB=tongDiem/tongTC
@@ -12,20 +11,28 @@ def tinhDiemTB(dsDiem,dsTC):
 def bangDiem(soSV):
     list=[0]*soSV
     for i in range(soSV):
-        list[i]=[" ",0,0,0]
+        list[i]=[" ",0,0,0]          #["Ten SV", diemTB, diem ren luyen, tong diem]
     return list
 # Ham loc ra danh sach nhan hoc bong
-def hocBong(SV):
+def hocBong(SV,n):
     ds=[]
-    while len(ds)<5:   #so luong hoc sinh nhan hoc bong
-        M=max(SV,key=lambda person : person[3])
-        ds.append(M)
-        SV.remove(M)
+    try:
+        while len(ds)<n:
+            M=max(SV,key=lambda person : person[1])
+            ds.append(M)
+            SV.remove(M)
+    except:
+        while len(ds)<len(SV):
+            M = max(SV, key=lambda person: person[1])
+            ds.append(M)
+            SV.remove(M)
     return ds
-tenMon=["Toan cao cap","Gioi thieu nganh"]
+
+tenMon=["Toan cao cap","Gioi thieu nganh"] #,"Tu duy lap trinh","Kinh te vi mo","LLNN va PL","Tu tuong HCM"]
+dsTC=[3,2] #,3,3,3,2]               #So tin chi moi mon
 soSV=int(input("Nhap so sinh vien: "))
+n=int(input("Nhap so sinh vien nhan hoc bong: "))
 SV=bangDiem(soSV)
-dsTC=[3,2]    # danh sách so tin chi moi mon
 #Nhap tên va diem sinh vien
 for i in range(soSV):
     SV[i][0]=input("Nhap ten sinh vien: ")
@@ -36,9 +43,11 @@ for i in range(soSV):
     SV[i][2]=int(input("Nhap diem ren luyen: "))
     SV[i][3]=SV[i][1]+0.2*SV[i][2]
 #In danh sach sinh vien nhan hoc bong
-dsHocBong=hocBong(SV)
+dsHocBong=hocBong(SV,n)
 print("Danh sach nhan hoc bong la:")
-for i in range(5):
-    print(dsHocBong[i][0])
+for i in range(len(dsHocBong)):
+    print(str(i+1)+". "+str(dsHocBong[i][0])+" : "+str(round(dsHocBong[i][3],2)))
+
+
 
 
